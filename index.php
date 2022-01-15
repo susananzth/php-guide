18b1f5c14fbe1491ddda1961ffb6883c229092f3
Susana Piñero <susananzth@gmail.com>
2022-01-15 17:11:51 -0500

    Agregando estilos, editando licencia.

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+	<!-- Etiquetas meta -->
+	<meta charset="utf-8">
+	<meta name="DC.Language" scheme="RFC1766" content="Spanish">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description"
+		content="Mi guía de php puro. Desarrollado por Susana Piñero @susananzth" />
+	<meta name="keywords" content="PHP" />
+	<meta name="author" content="Susana Piñero Rodríguez" />
+	<meta name="copyright" content="Susana Piñero Rodríguez" />
+	<meta name="reply-to" content="me@susananzth.com">
+	<link rev="made" href="mailto:me@susananzth.com">
+	<!-- Título de la página -->
+	<title>My Guide PHP | SusanaNzth</title>
+	<!-- Ícono -->
+	<link rel="icon" type="image/x-icon" href="favicon.ico">
+	<!-- Fuentes -->
+	<link rel="stylesheet" href="css/bootstrap-icons.css">
+	<!-- Estilos -->
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body class="d-flex flex-column">
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="#">
+					<img src="img/logo.png" alt="Logo de SusanaNzth" width="30" height="24" class="d-inline-block align-text-top">
+					MyGuide</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+					data-bs-target="#navbarMyGuide" aria-controls="navbarMyGuide"
+					aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<!-- Barra de navegacion -->
+				<div class="collapse navbar-collapse" id="navbarMyGuide">
+					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+						<li class="nav-item">
+							<a class="nav-link active" aria-current="page" href="#">Inicio</a>
+						</li>
+					</ul>
+					<form class="d-flex">
+						<input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
+						<button class="btn btn-outline-success" type="submit">Buscar</button>
+					</form>
+				</div>
+			</div>
+		</nav>
+	</header>
+	<main>
+		<div class="container-fluid">
+			<div class="row text-center">
+				
+			</div>
+		</div>
+	</main>
+	<footer class="footer mt-auto py-3 bg-light">
+		<div class="container">
+			<span class="text-muted">Susana Piñero Rodríguez @susananzth</span>
+		</div>
+	</footer>
+	<script src="js/bootstrap.bundle.min.js"></script>
+	<script src="js/jquery-3.6.0.min.js"></script>
+	<script src="js/scripts.js"></script>
+</body>
+</html>
